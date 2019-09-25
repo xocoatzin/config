aedb5b82d8e613f57f37079fee0017522a538403
Alan Torres <atorresgomez@magicleap.com>
2019-09-25 14:34:10 +0200

    Update configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar main &
-polybar bottom &
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main &
+        #MONITOR=$m polybar --reload bottom &
+    done
+else
+    polybar --reload main &
+    #polybar --reload bottom &
+fi
 
 echo "Polybar launched..."
