68a74f79404ddecb28704abf053e2969d36a0265
Alan Torres <atorresgomez@magicleap.com>
2022-07-03 01:07:05 +0200

    LSP

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -270,11 +270,11 @@ return require("packer").startup(function()
 			vim.g.gruvbox_baby_telescope_theme = 1
 			vim.g.gruvbox_baby_highlights = {
 				Visual = { bg = "#444455" },
-				CocGitAddedSign = { fg = colors.soft_green },
-				CocGitChangedSign = { fg = colors.soft_yellow },
-				CocGitRemovedSign = { fg = colors.red },
-				CocGitTopRemovedSign = { fg = colors.red },
-				CocGitChangeRemovedSign = { fg = colors.red },
+				-- CocGitAddedSign = { fg = colors.soft_green },
+				-- CocGitChangedSign = { fg = colors.soft_yellow },
+				-- CocGitRemovedSign = { fg = colors.red },
+				-- CocGitTopRemovedSign = { fg = colors.red },
+				-- CocGitChangeRemovedSign = { fg = colors.red },
 			}
 			vim.cmd([[colorscheme gruvbox-baby]])
 		end,
@@ -297,13 +297,210 @@ return require("packer").startup(function()
 			})
 		end,
 	})
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-calc" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-cmdline" },
+			{ "hrsh7th/cmp-emoji" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+			{ "f3fora/cmp-spell" },
+			{ "L3MON4D3/LuaSnip" },
+			{ "saadparwaiz1/cmp_luasnip" },
+		},
+		config = function()
+			-- Setup nvim-cmp.
+			local cmp = require("cmp")
+
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					-- Accept currently selected item.
+					-- Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "calc" },
+					{ name = "spell" },
+					{ name = "path" },
+					{ name = "emoji" },
+					{ name = "nvim_lua" },
+					{ name = "nvim_lsp_signature_help" },
+				}, {}),
+			})
+
+			-- Completions for command mode
+			require("cmp").setup.cmdline(":", {
+				sources = {
+					{ name = "cmdline" },
+				},
+			})
+
+			-- Completions for / search based on current buffer
+			require("cmp").setup.cmdline("/", {
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Set configuration for specific filetype.
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+
+			-- Setup lspconfig.
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+			-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+			-- require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
+			-- 	capabilities = capabilities,
+			-- })
+		end,
+	})
+	use({
+		"neovim/nvim-lspconfig",
+		requires = {
+			{ "williamboman/nvim-lsp-installer" },
+		},
+		config = function()
+			-- Mappings.
+			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+			local opts = { noremap = true, silent = true }
+			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+
+			-- Use an on_attach function to only map the following keys
+			-- after the language server attaches to the current buffer
+			local on_attach = function(client, bufnr)
+				-- Enable completion triggered by <c-x><c-o>
+				vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+				-- Mappings.
+				-- See `:help vim.lsp.*` for documentation on any of the below functions
+				local bufopts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+				vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+				vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+				vim.keymap.set("n", "<space>wl", function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end, bufopts)
+				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+				vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+				vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+				vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
+			end
+
+			local lsp_flags = {
+				-- This is the default in Nvim 0.7+
+				debounce_text_changes = 150,
+			}
+
+			require("nvim-lsp-installer").setup({
+				automatic_installation = true,
+			})
+
+			local lsp = require("lspconfig")
+			lsp.pyright.setup({
+				on_attach = on_attach,
+				flags = lsp_flags,
+			})
+			lsp.tsserver.setup({
+				on_attach = on_attach,
+				flags = lsp_flags,
+			})
+			lsp.rust_analyzer.setup({
+				on_attach = on_attach,
+				flags = lsp_flags,
+				-- Server-specific settings...
+				settings = {
+					["rust-analyzer"] = {},
+				},
+			})
+		end,
+	})
 	-- Vimscript plugins
 	-- use({ "flazz/vim-colorschemes" })
-	use({
-		"neoclide/coc.nvim",
-		branch = "release",
-		run = "yarn install --frozen-lockfile",
-	})
+	-- use({
+	-- 	"neoclide/coc.nvim",
+	-- 	branch = "release",
+	-- 	run = "yarn install --frozen-lockfile",
+	-- })
 	use({ "flwyd/vim-conjoin" })
 	use({ "ggandor/lightspeed.nvim" })
 	use({ "github/copilot.vim", disable = true })
